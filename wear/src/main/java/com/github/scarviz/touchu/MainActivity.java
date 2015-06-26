53fe69123bc52aefd65f4ee88ab2bc4e45d04078
scarviz <scarviz@gmail.com>
2015-06-26 23:27:47 +0900

    add touchu project

---
+++ wear/src/main/java/com/github/scarviz/touchu/MainActivity.java
@@ -0,0 +1,70 @@
+package com.github.scarviz.touchu;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends WearableActivity {
+    private static final String TAG = "MainActivity";
+
+    private BoxInsetLayout mContainerView;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setAmbientEnabled();
+
+        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+        updateDisplay();
+    }
+
+    @Override
+    public void onUpdateAmbient() {
+        super.onUpdateAmbient();
+        updateDisplay();
+    }
+
+    @Override
+    public void onExitAmbient() {
+        updateDisplay();
+        super.onExitAmbient();
+    }
+
+    private void updateDisplay() {
+        if (isAmbient()) {
+            mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
+            mTextView.setTextColor(getResources().getColor(android.R.color.white));
+        } else {
+            mContainerView.setBackground(null);
+            mTextView.setTextColor(getResources().getColor(android.R.color.black));
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.d(TAG, "dispatchTouchEvent");
+        switch (event.getAction()) {
+            // 画面タッチ時
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "dispatchTouchEvent : ACTION_DOWN");
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+}
